31d93d9e1564d5593bd8306db0b12653f8bbfa5f
chrishoen <chris123hoen@gmail.com>
2022-01-23 11:18:38 -0500

    more CMakeSettings.json

--- MyGlobalImports.cmake
+++ MyGlobalImports.cmake
@@ -5,10 +5,10 @@
 function(my_init_global_import_variables)
 
    if(MSVC)
-      set (MyRisLibIncludePath "C:\\MyTools\\MyLib\\include\\RisLib" PARENT_SCOPE)
-      set (MyRisLibImportPath  "C:\\MyTools\\MyLib\\lib\\RisLib.lib" PARENT_SCOPE)
-      set (MyDspLibIncludePath "C:\\MyTools\\MyLib\\include\\DspLib" PARENT_SCOPE)
-      set (MyDspLibImportPath  "C:\\MyTools\\MyLib\\lib\\DspLib.lib" PARENT_SCOPE)
+      set (MyRisLibIncludePath "C:/MyTools/MyLib/include/RisLib" PARENT_SCOPE)
+      set (MyRisLibImportPath  "C:/MyTools/MyLib/lib/RisLib.lib" PARENT_SCOPE)
+      set (MyDspLibIncludePath "C:/MyTools/MyLib/include/DspLib" PARENT_SCOPE)
+      set (MyDspLibImportPath  "C:/MyTools/MyLib/lib/DspLib.lib" PARENT_SCOPE)
    elseif(CMAKE_SYSTEM_VERSION EQUAL 101)
       set (MyRisLibIncludePath "C:/MyTools/MyLib/include/RisLib" PARENT_SCOPE)
       set (MyRisLibImportPath  "C:/MyTools/MyLib/lib/libRisLib.so" PARENT_SCOPE)
@@ -30,6 +30,14 @@ endfunction()
 function(my_lib_import_RisLib _target)
 
    if (MSVC)
+      set (MyRisLibImportPath  "C:/MyTools/MyLib/lib/RisLib.lib")
+   elseif (MYMODE STREQUAL beagle)
+      set (MyRisLibImportPath  "C:/MyTools/MyLib/lib/libRisLib.so")
+   else()
+      set (MyRisLibImportPath  "/opt/prime/lib/libRisLib.so")
+   endif()
+
+   if (MSVC)
       add_library(RisLib STATIC IMPORTED)
       set_target_properties(RisLib PROPERTIES IMPORTED_LOCATION ${MyRisLibImportPath})
       target_link_libraries(RisLib INTERFACE ws2_32)
@@ -41,11 +49,18 @@ function(my_lib_import_RisLib _target)
       target_link_libraries(${_target} RisLib)
       target_link_libraries(${_target} pthread)
       target_link_libraries(${_target} rt)
+      target_link_libraries(${_target} readline)
    endif()
 
 endfunction()
 
 function(my_inc_import_RisLib _target)
+
+   if (MSVC OR MYMODE STREQUAL beagle)
+      set (MyRisLibIncludePath "C:/MyTools/MyLib/include/RisLib")
+   else()
+      set (MyRisLibIncludePath "/opt/prime/include/RisLib")
+   endif()
 
    target_include_directories(${_target} PUBLIC ${MyRisLibIncludePath})
 
@@ -58,7 +73,15 @@ endfunction()
 
 function(my_lib_import_DspLib _target)
 
-   add_library(DspLib STATIC IMPORTED)
+   if (MSVC)
+      set (MyDspLibImportPath  "C:/MyTools/MyLib/lib/DspLib.lib")
+   elseif (MYMODE STREQUAL beagle)
+      set (MyDspLibImportPath  "C:/MyTools/MyLib/lib/libDspLib.so")
+   else()
+      set (MyDspLibImportPath  "/opt/prime/lib/libDspLib.so")
+   endif()
+
+   add_library(DspLib SHARED IMPORTED)
    set_target_properties(DspLib PROPERTIES IMPORTED_LOCATION ${MyDspLibImportPath})
 
    target_link_libraries(${_target} DspLib)
@@ -66,6 +89,12 @@ function(my_lib_import_DspLib _target)
 endfunction()
 
 function(my_inc_import_DspLib _target)
+
+   if (MSVC OR MYMODE STREQUAL beagle)
+      set (MyDspLibIncludePath "C:/MyTools/MyLib/include/DspLib")
+   else()
+      set (MyDspLibIncludePath "/opt/prime/include/DspLib")
+   endif()
 
    target_include_directories(${_target} PUBLIC ${MyDspLibIncludePath})
 
