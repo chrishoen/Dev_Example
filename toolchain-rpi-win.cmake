3306fb6f4090efb27d9586f653d30819e0c9fcda
chrishoen <chris123hoen@gmail.com>
2024-07-21 13:02:25 -0400

    toolchain-rpi-win

---
+++ toolchain-rpi-win.cmake
@@ -0,0 +1,19 @@
+# this one is important
+SET(CMAKE_SYSTEM_NAME Linux)
+
+# specify the cross compiler
+SET(CMAKE_C_COMPILER
+C:/MyDev/toolchain-rpi/bin/aarch64-linux-gnu-gcc.exe)
+SET(CMAKE_CXX_COMPILER
+C:/MyDev/toolchain-rpi/bin/aarch64-linux-gnu-g++.exe)
+# where is the target environment
+SET(CMAKE_FIND_ROOT_PATH
+C:/MyDev/toolchain-rpi)
+# for cmake 3.8
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
